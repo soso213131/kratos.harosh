205d48174768a5e99c0d4ce33a67a87a523eaa4f
soso213131 <negru.gandonu@gmail.com>
2025-11-11 20:53:34 +0200

    Add files via upload

---
+++ Ernest.py
@@ -0,0 +1,33 @@
+player_age = 488
+quests_done = 5
+player_position = (1, 1)
+player_state = 'In Battle'
+current_quest = 'Dragon Hunt'
+attack_range = (10, 12)
+player_score = 12
+event_time = (1, 12)
+main_goal = 'Become a Hero'
+player_level = 211
+
+
+print(player_age)
+print(quests_done)
+print(player_position)
+print(player_state)
+print(current_quest)
+print(attack_range)
+print(player_score)
+print(event_time)
+print(main_goal)
+print(player_level)
+
+print(type(player_age))
+print(type(quests_done))
+print(type(player_position))
+print(type(player_state))
+print(type(current_quest))
+print(type(attack_range))
+print(type(player_score))
+print(type(event_time))
+print(type(main_goal))
+print(type(player_level))
